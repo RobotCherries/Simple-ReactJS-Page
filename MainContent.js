545b870690ae8120880f6e80951055c1670bfedb
RobotCherries <zdip96@gmail.com>
2018-12-05 23:57:39 +0200

    Initial commit

---
+++ MainContent.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+function MainContent() {
+    return(
+        <main>
+            <article>
+                <h2>First Article</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mi nisl, hendrerit vitae pellentesque quis, vehicula ac ipsum. Aenean imperdiet ligula eget turpis varius lobortis. Vivamus est erat, eleifend eget pretium id, sollicitudin eu magna. Maecenas sit amet vulputate est.</p>
+            </article>
+
+            <article>
+                <h2>Second Article</h2>
+                <p>Fusce egestas turpis justo, ac dapibus dolor fringilla ut. Sed id ex et sem tempor pretium. Donec iaculis sapien iaculis, lobortis nulla at, tempus dolor. Fusce tristique massa et aliquet eleifend. Nulla sed pulvinar ex. Etiam ipsum metus, commodo in fermentum a, mollis at neque. Donec molestie urna tellus, eget viverra lorem congue eu.</p>
+            </article>
+        </main>
+    )
+}
+
+export default MainContent
